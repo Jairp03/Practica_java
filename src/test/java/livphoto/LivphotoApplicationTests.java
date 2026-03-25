265594c06384e2dcd13213f97891223474d91199
Jairp03 <elvismartillo64@gmail.com>
2026-03-24 21:01:33 -0500

    first commit

---
+++ src/test/java/livphoto/LivphotoApplicationTests.java
@@ -0,0 +1,13 @@
+package livphoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivphotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
